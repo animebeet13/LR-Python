62f8cd6937e6f796b08cff6c215e05158417ee85
animebeet13 <nikita2004666@gmail.com>
2024-10-17 16:59:00 +0300

    Add files via upload

---
+++ Лабораторная работа 1/task_3.py
@@ -0,0 +1,7 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+first_team = list_players[:len(list_players)//2]
+second_team = list_players[len(list_players)//2:]
+
+print(first_team)
+print(second_team)
